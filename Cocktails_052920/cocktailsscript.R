52025bd2c7fe09dda1f2c2263cf4e505a7574329
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2020-05-26 21:05:18 -0700

    minor edits to code

--- Cocktails_052920/cocktailsscript.R
+++ Cocktails_052920/cocktailsscript.R
@@ -23,23 +23,21 @@ ingreds<-cocktails %>%
  # select(!ingredient_number) %>%
   filter(`Bitters` == 1)  # only pull out drinks with bitters
   
+# pull out only the ingredients for the nmds code
 ingreds.only<-ingreds %>%
   ungroup() %>%
   select(4:309) %>%
   select_if(colSums(.) != 0) # only pull out columns without all zeros
-
-
-## filter out all drinks that have bitters in them
 
 # run an nMDS 
 ingreds.mds <- metaMDS(ingreds.only, distance = "bray")
 
 #Each dot represents a drink. Dots closer together means ingredients within that drink are more similar
 # 
-spp.ingred<- data.frame(ingreds.mds$species) #save species intrinsic values into dataframe
+spp.ingred<- data.frame(ingreds.mds$species) #save "species" intrinsic values into dataframe
 spp.ingred <- cbind(spp.ingred, Species = rownames(spp.ingred)) #add species names to dataframe
 
-sites.ingred<- data.frame(ingreds.mds$points) #save species intrinsic values into dataframe
+sites.ingred<- data.frame(ingreds.mds$points) #save "site" intrinsic values into dataframe
 sites.ingred<-sites.ingred %>%
   bind_cols(ingreds) %>%
   select(drink, glass, category, MDS1, MDS2)
@@ -51,8 +49,7 @@ nmds.plot <- ggplot(# run an nMDS
   theme_void()+ 
   xlab("")+
   ylab("")+
-#   ggtitle("Drinks with bitters as an ingredient") +
-  scale_color_fish_d(option = "Naso_lituratus")+
+  scale_color_fish_d(option = "Naso_lituratus")+ # fish color palette!
   theme(panel.background = element_rect(fill = "seashell2", colour = "black", size = 1, linetype = "solid"),
         legend.position = "right", 
         legend.text = element_text(size = 12), 
@@ -64,8 +61,8 @@ nmds.plot <- ggplot(# run an nMDS
 # vectors for "species" (ingredient)
 species<-nmds.plot + 
   ggrepel::geom_text_repel(data = sites.ingred, aes(x=MDS1, y=MDS2, label = drink), cex = 3, direction = "both", segment.size = 0.25)+
-  geom_point(aes(MDS1, MDS2, color = glass), size = 3)+ #adds site points to plot, shape determined by Landuse, colour determined by Management
-  labs(colour = "Glass Type") # add legend labels for Management and Landuse
+  geom_point(aes(MDS1, MDS2, color = glass), size = 3)+ #adds site points to plot
+  labs(colour = "Glass Type") # add legend labels 
   
 # points for "sites" (drink name)  
 sites<-nmds.plot +
@@ -73,7 +70,7 @@ sites<-nmds.plot +
                arrow = arrow(length = unit(0.25, "cm")), colour = "grey10", lwd=0.3) + #add vector arrows of ingredients
   ggrepel::geom_text_repel(data = spp.ingred, aes(x=MDS1, y=MDS2, label = Species), cex = 3, direction = "both", segment.size = 0.25)
 
-# put the plots together in patchword
+# put the plots together in patchwork
 finalplot<-sites+species+
   plot_annotation(title = "Drinks with bitters as an ingredient") &
   theme(plot.title = element_text(hjust = 0.5), legend.position = "bottom")&
